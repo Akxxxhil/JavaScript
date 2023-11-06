b49088b4aa13aef8caf576381c4631c33e7fb067
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-06 21:24:35 +0530

    date and time in js

---
+++ DateandTime.js
@@ -0,0 +1,9 @@
+let mydate=new Date()
+// console.log(mydate.toString()) //  this is for the latest date of today
+console.log(mydate.getFullYear())
+
+
+
+
+// console.log(typeof (mydate));  
+// ==>>> it will show as the object
