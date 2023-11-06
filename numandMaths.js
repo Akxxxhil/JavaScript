53a064dde6be2f59d4f7ff6f230e5f7561faa859
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-06 20:40:03 +0530

    Numbers and Math in Js

---
+++ numandMaths.js
@@ -0,0 +1,21 @@
+const score=400
+console.log(score)
+
+const balance =new Number(100)
+console.log(balance)
+
+// .toString()
+// it is used for convert into string
+
+//toFixed(2)
+//convert into prcise value(2)
+
+//toPrecison 
+//it gives round up value
+
+//toLocaleString('en-IN')
+// gives answer in the indian standard value
+
+// Math.abs ==>absolute value in math
+// Math.floor ++> lest value and ceil for high value
+// Math.floor is used for round up the value in 1 lower round up...
