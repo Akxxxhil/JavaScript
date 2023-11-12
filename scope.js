14d23a9604df3c1e5071e70ada40543efe363acc
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-12 08:29:27 +0530

    basics of scope

---
+++ scope.js
@@ -0,0 +1,8 @@
+if(true){
+   let a =40;
+   const k=90;
+   var g=909;
+}
+// console.log(a); ==>> error beacuse its not in scope;
+// console.log(k); ==>> error beacuse its not in scope;
+// console.log(g); ==it will print ...so never use var
