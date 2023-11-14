42e8d0a085ac62953be7306eeaf541d35afba8ee
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-14 16:08:40 +0530

    control flow

---
+++ controlflow1.js
@@ -0,0 +1,22 @@
+const temperature=400
+
+// if(temperature< 90){
+//     console.log("yes we can live");
+// }
+// else {
+//     console.log("no we can't");
+// }
+// console.log("yes code executes well");
+
+if(temperature >40 && temperature<100){
+    console.log("its too hot");
+}
+else if (temperature >100){
+    console.log("you cant leave");
+}
+else if(temperature<10){
+    console.log("its too cool");
+}
+else {
+    console.log("its ok ok");
+}
