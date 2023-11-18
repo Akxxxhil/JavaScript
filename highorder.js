5d2c3609ecf41552e6ac439b80d852007344f5a6
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-18 19:09:11 +0530

    forloop loop

---
+++ highorder.js
@@ -0,0 +1,21 @@
+const arr=[1,2,34,5,6,7,]
+
+for (const iterator of arr) {
+    //console.log(`the number in this array is ${iterator}`);
+}
+
+//this for of loop indicates that we can  itraete through an array
+
+
+const map=new Map()
+
+map.set(1,'india')
+map.set(2,'aus')
+map.set(3,'greece')
+
+for (const [iterator,value] of map) {
+   // console.log(iterator,value);
+}
+
+// we are using  forof loop for the map
+
