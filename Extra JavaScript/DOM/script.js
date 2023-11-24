ff70e2ec943085a1363f74810420e83df4453edc
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-11-24 15:50:41 +0530

    extra practice

---
+++ Extra JavaScript/DOM/script.js
@@ -0,0 +1,14 @@
+// question 1
+// const doc=document.getElementById('heading')
+// doc.append("from Akxxhil")
+
+
+// question 2
+
+let newVar=document.querySelectorAll('.box');
+console.log(newVar)
+newVar[0].innerText="hi everyone i am innertext1"
+newVar[1].innerText="hi everyone i am innertext2"
+newVar[2].innerText="hi everyone i am innertext3"
+
+console.log(newVar);
