44d8f945545d5d494eaac19fd1b0de172045bd62
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2023-12-14 21:06:28 +0530

    Basics of Data Modeling

---
+++ BackEnd/class 3/todos/todo.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+const todoSchema=new mongoose.Schema(
+    {
+        content:{
+            type:String,
+            required:true
+        },
+        complete:{
+            type:Boolean,
+            required:true
+        },
+        createdBy:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        subTodos: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: 'SubTodo',
+            },
+          ],
+    }
+    ,{timestamps:true})
+export const TODO=mongoose.model("TODO",todoSchema)
